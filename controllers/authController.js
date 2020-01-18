0cbb3a8868da2a3c5a2a5a3926c20f674fdbe272
brandenlacour <brandenlacour@gmail.com>
2020-01-18 15:53:05 -0600

    added user creation

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,13 +1,24 @@
 const mongoose = require('mongoose')
 const express = require('express')
 const router = express.Router()
-
+const User = require('../models/user')
 
 
 router.get('/register', (req, res) => {
-	
+	res.render('auth/new.ejs')
 })
 
+router.post('/register', async (req, res, next) => {
+
+	try {
+		const createdUser = await User.create(req.body)
+		res.redirect('/')
+	} catch(err) {
+		next(err)
+
+	}
+	
+})
 
 
 
