332cd167a345ae14566170d8e611b3ca0bd388c3
brandenlacour <brandenlacour@gmail.com>
2020-01-18 14:45:40 -0600

    added folder structure

---
+++ server.js
@@ -0,0 +1,36 @@
+require('dotenv').config()
+const express = require('express')
+const app = express()
+const bodyParser = require('body-parser')
+const PORT = process.env.PORT
+
+
+//middleware
+app.use(bodyParser.urlencoded({extended: false}))
+app.use(express.static('public'))
+
+
+app.get('/', (req, res) => {
+	res.send('At home');
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+app.listen(PORT, () => {
+	console.log(`App is listening on port ${PORT}`);
+})
