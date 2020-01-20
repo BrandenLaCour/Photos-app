8c712920ba5e0f7658bcff140af10358831c279b
brandenlacour <brandenlacour@gmail.com>
2020-01-20 12:02:26 -0600

    added logout

--- controllers/authController.js
+++ controllers/authController.js
@@ -5,94 +5,82 @@ const User = require('../models/user')
 
 
 router.get('/register', (req, res) => {
-	const message = req.session.message
-	req.session.message = ''
-	res.render('auth/new.ejs', {message: message})
+    const message = req.session.message
+    req.session.message = ''
+    res.render('auth/new.ejs', { message: message })
 })
 
 router.post('/register', async (req, res, next) => {
 
-	try {
-		const foundUser = await User.findOne({username: req.body.username})
-		if (!foundUser){
-			const createdUser = await User.create(req.body)
-			req.session.message = 'Thanks for creating an account!'
-			res.redirect('/')
-				
-		}
-		else {
-			req.session.message = 'Username already taken'
-			res.redirect('/auth/register')
-		}
-		
-	} catch(err) {
-		next(err)
-	}
-	
+    try {
+        const foundUser = await User.findOne({ username: req.body.username })
+        if (!foundUser) {
+            const createdUser = await User.create(req.body)
+            req.session.message = 'Thanks for creating an account!'
+            res.redirect('/')
+
+        } else {
+            req.session.message = 'Username already taken'
+            res.redirect('/auth/register')
+        }
+
+    } catch (err) {
+        next(err)
+    }
+
 })
 
 
 router.get('/login', async (req, res) => {
-	const message = req.session.message
-	res.render('auth/login.ejs', {message: message})
+    const message = req.session.message
+    res.render('auth/login.ejs', { message: message })
 })
 
 
 
 router.post('/login', async (req, res, next) => {
 
-	try {
-		const loginAttempt = req.body
-		const foundUser = await User.findOne({username: loginAttempt.username})
-	  // store current user login,
-	  	if (foundUser){
-	  		//do check on password
-	  		if (foundUser.password === loginAttempt.password){
-	  		//grant auth
-	  			req.session.message = `Welcome ${foundUser.username}`
-	  			req.session.userId = foundUser._id
-	  			req.session.username = foundUser.username
-	  			res.redirect('/')
+    try {
+        const loginAttempt = req.body
+        const foundUser = await User.findOne({ username: loginAttempt.username })
+        // search for that username request, 
+        // store current user login,
+        if (foundUser) {
+            // if there is a match, compare passwords
+            if (foundUser.password === loginAttempt.password) {
+                //grant auth
+                req.session.message = `Welcome ${foundUser.username}`
+                req.session.userId = foundUser._id
+                req.session.username = foundUser.username
+                res.redirect('/')
 
-	  		}
-	  		else{
-	  			console.log('Password is incorrect');
-	  			req.session.message = 'Username or Password is incorrect'
-	  			res.redirect('/auth/login')
-	  		}
-
-
-
-	  	}
-	  	else {
-	  		console.log('Username is incorrect');
-	  		req.session.message = 'Username or Password incorrect'
-	  		res.redirect('/auth/login')
-
-
-	  	}
-
-	 
-	  // search for that username request, 
-	  // if there is a match, compare passwords
-	  // if there is not match, return username or password is incorrect
-
-	  // if there is, compare passwords, if matched, redirect to home and update message to welcome,
-	  // if not, let them know that the username or pass is incorrect.
+            } else {
+            	//if password does not match 
+                console.log('Password is incorrect');
+                req.session.message = 'Username or Password is incorrect'
+                res.redirect('/auth/login')
+            }
 
 
 
+        } else {
+            // if there is not match, return username or password is incorrect
+            console.log('Username is incorrect');
+            req.session.message = 'Username or Password incorrect'
+            res.redirect('/auth/login')
+        }
 
-
-		
-	}
-	catch(err){
-		next(err)
-	}
+    } catch (err) {
+        next(err)
+    }
 
 })
 
-
+router.get('/logout', async (req, res) => {
+	
+	await req.session.destroy()
+	res.redirect('/login')
+})
 
 
 
