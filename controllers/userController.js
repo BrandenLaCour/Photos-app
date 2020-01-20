164ec2b2ea8a73b153b2ccaa6717162b29509fb3
brandenlacour <brandenlacour@gmail.com>
2020-01-20 15:41:46 -0600

    user can be deleted if logged in

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,14 +1,14 @@
 const express = require('express')
 const router = express.Router()
 const User = require('../models/user')
-
+const Photo = require('../models/photo')
 //index page
 router.get('/', async (req, res, next) => {
 	const message = req.session.message
 	req.session.message = ''
 
 	try {
-		console.log('ran users find');
+	
 		const usersFound = await User.find({})
 		res.render('users/index.ejs', {users: usersFound, message: message})
 	}
@@ -25,7 +25,7 @@ router.get('/:id', async (req, res, next) => {
 	try {
 		//find user, then access photos array
 		const foundUser = await User.findById(req.params.id).populate('photos')
-		res.render('users/show.ejs', {user: foundUser, photos: foundUser.photos})
+		res.render('users/show.ejs', {user: foundUser, photos: foundUser.photos, username: req.session.username})
 	}
 	catch(err){
 		next(err)
@@ -33,9 +33,22 @@ router.get('/:id', async (req, res, next) => {
 	}
 })
 
+//delete
 
-
-
+router.delete('/:id', async (req, res, next) => {
+	
+	try {
+		//find user, delete all photos associated with user, then delete user
+		const foundUser = await User.findById(req.params.id)
+		const removedPhotos = await Photo.remove({user: foundUser.username})
+		const removedUser = await User.findByIdAndRemove(req.params.id)
+		req.session.message = 'Account has been deleted'
+		res.redirect('/')
+	}
+	catch(err){
+		next(err)
+	}
+})
 
 
 
